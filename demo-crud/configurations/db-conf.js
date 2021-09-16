4a4160d28274a88e3fa1f58f7affefb0ba3a4fb2
DaniaFHG <daniafernanda.15@gmail.com>
2021-09-15 19:15:07 -0600

    cambios

--- demo-crud/configurations/db-conf.js
+++ demo-crud/configurations/db-conf.js
@@ -1,7 +1,7 @@
-const host="j8oay8teq9xaycnm.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-const user="glv28hixbmm2knb2";
-const password="ll9rlfcdlp2jl3sl";
-const database="ospjfp9qxlayegmj";
+const host="nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+const user="onztirl2bt98eb5m";
+const password="b2bnak8vpmkrie1o";
+const database="zr0qgoqrnfqyemn0";
 
 exports.host=host;
 exports.user=user;
